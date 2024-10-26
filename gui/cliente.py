52f2f787d61863a7c73a177762f3473d0bb6123f
Marcos22 <marcoscufre04@gmail.com>
2024-10-26 16:41:23 -0300

    soy cufre

--- gui/cliente.py
+++ gui/cliente.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMessageBox,QTableWidgetItem
+from PyQt5.QtWidgets import QMessageBox, QTableWidgetItem
 from PyQt5.QtCore import QDate
 from src.conexion import Conexion
 
@@ -16,7 +16,7 @@ class Clientes():
         query="SELECT day(GETDATE()),month(GETDATE()),year(GETDATE())"
         res = self.cursor.execute(query)
         fecha_hoy= res.fetchall()
-        self.cliente.dtpFechaRegistro.setDate(QDate(fecha_hoy[0][2],fecha_hoy[0][1],fecha_hoy[0][0]))
+        self.cliente.dtpFechaRegistro.setDate(QDate(fecha_hoy[0][2], fecha_hoy[0][1], fecha_hoy[0][0]))
         self.cargar_datos_cliente()
         self.cliente.btnGuardar.clicked.connect(self.nuevo_cliente)
         
@@ -27,12 +27,12 @@ class Clientes():
         self.cliente.tblClientes.setRowCount(len(datos_clientes))
         fila = 0
         for item in datos_clientes:
-            self.cliente.tblClientes.setItem(fila,0,QTableWidgetItem(str(item[0])))
-            self.cliente.tblClientes.setItem(fila,1,QTableWidgetItem(str(item[1])))
-            self.cliente.tblClientes.setItem(fila,2,QTableWidgetItem(str(item[2])))
-            self.cliente.tblClientes.setItem(fila,3,QTableWidgetItem(str(item[3])))
-            self.cliente.tblClientes.setItem(fila,4,QTableWidgetItem(str(item[4])))
-            self.cliente.tblClientes.setItem(fila,5,QTableWidgetItem(str(item[5])))
+            self.cliente.tblClientes.setItem(fila, 0, QTableWidgetItem(str(item[0])))
+            self.cliente.tblClientes.setItem(fila, 1, QTableWidgetItem(str(item[1])))
+            self.cliente.tblClientes.setItem(fila, 2, QTableWidgetItem(str(item[2])))
+            self.cliente.tblClientes.setItem(fila, 3, QTableWidgetItem(str(item[3])))
+            self.cliente.tblClientes.setItem(fila, 4, QTableWidgetItem(str(item[4])))
+            self.cliente.tblClientes.setItem(fila, 5, QTableWidgetItem(str(item[5])))
             fila +=1
             
     def valid(self):
@@ -44,11 +44,11 @@ class Clientes():
             correo = self.cliente.txtCorreo.text()
             telefono = self.cliente.txtTelefono.text()
             direccion = self.cliente.txtDireccion.text()
-            fechaRegistro = self.cliente.dtpFechaRegistro.date().toString("yyyy-MM-dd")
+            fecharegistro = self.cliente.dtpFechaRegistro.date().toString("yyyy-MM-dd")
             
             query = "INSERT INTO Clientes (nombre, correo, telefono, direccion, fecha_registro) VALUES(?,?,?,?,?)"
-            values = (nombre,correo,telefono,direccion,fechaRegistro)
-            self.cursor.execute(query,values)
+            values = (nombre, correo, telefono, direccion, fecharegistro)
+            self.cursor.execute(query, values)
             self.cursor.commit()
             print("Se creo un nuevo cliente")
         except Exception as e:
--- src/conexion.py
+++ src/conexion.py
@@ -3,7 +3,7 @@ import pyodbc
 class Conexion():
     def __init__(self):
         try:
-            self.con = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-B2OVTGG;DATABASE=AGENCIA_AC;Trusted_Connection=Yes;')
+            self.con = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-PM1QNE7\SQLEXPRESS;DATABASE=AGENCIA_AC;Trusted_Connection=Yes;')
         except Exception as e:
             print(e)
         

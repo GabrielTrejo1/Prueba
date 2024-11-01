9233b373a77cf4bcdf2fde3357ab0e12aba18bbb
aletondaX <gtrrsta@outlook.com>
2024-11-01 19:11:35 -0300

    Limpieza de código

--- src/controllers/ventas.py
+++ src/controllers/ventas.py
@@ -27,84 +27,7 @@ class Ventas():
     self.ventas.tblVehiculos.cellDoubleClicked.connect(self.seleccionar_vehiculo)
     self.ventas.btnGuardar.clicked.connect(self.guardar_venta)
     
-  # poblar la tabla de Ventas
-  def cargar_ventas(self): 
-    try:
-      query = "SELECT Vent.ID, C.nombre, Vent.precio_final, Vent.fecha_venta, Vehi.marca, Vehi.modelo, Vehi.dominio FROM VENTAS AS Vent INNER JOIN CLIENTES AS C ON Vent.id_cliente = C.ID INNER JOIN VEHICULOS AS Vehi ON Vent.id_vehiculo = Vehi.ID ORDER BY Vent.ID DESC"
-      datos_ventas = self.db.execute_query_fetchall(query)
-      
-      self.ventas.tblVentas.setRowCount(len(datos_ventas))
-      fila = 0
-      for item in datos_ventas:
-        self.ventas.tblVentas.setItem(fila,0,QTableWidgetItem(str(item[0])))
-        self.ventas.tblVentas.setItem(fila,1,QTableWidgetItem(str(item[1])))
-        self.ventas.tblVentas.setItem(fila,2,QTableWidgetItem(f"{str(item[4])} {str(item[5])} ({str(item[6])})")) # Marca Modelo (Patente)
-        self.ventas.tblVentas.setItem(fila,3,QTableWidgetItem(str(item[2])))
-        self.ventas.tblVentas.setItem(fila,4,QTableWidgetItem(str(item[3])))
-        fila +=1
-      self.ventas.tblVentas.resizeColumnsToContents()
-    except Exception as e:
-      print("Error al cargar las ventas: ", e)
-
-  # poblar la tabla de Clientes
-  def cargar_clientes(self):
-    try:
-      query = "SELECT id, nombre, dni FROM CLIENTES ORDER BY id DESC"
-      datos_clientes = self.db.execute_query_fetchall(query)
-      self.ventas.tblClientes.setRowCount(len(datos_clientes))
-      for fila, item in enumerate(datos_clientes):
-        for columna, valor in enumerate(item):
-          self.ventas.tblClientes.setItem(fila, columna, QTableWidgetItem(str(valor)))
-      self.ventas.tblClientes.resizeColumnsToContents()
-    except Exception as e:
-      print("Error al cargar los clientes: ", e)
-      
-  # poblar la tabla de Vehículos
-  def cargar_vehiculos(self):
-    try:
-      query = "SELECT ID, marca, modelo, color, dominio FROM VEHICULOS"
-      datos_vehiculos = self.db.execute_query_fetchall(query)
-      self.ventas.tblVehiculos.setRowCount(len(datos_vehiculos))
-      for fila, item in enumerate(datos_vehiculos):
-        for columna, valor in enumerate(item):
-          self.ventas.tblVehiculos.setItem(fila, columna, QTableWidgetItem(str(valor)))
-      self.ventas.tblVehiculos.resizeColumnsToContents()
-    except Exception as e:
-      print("Error al cargar los vehículos: ", e)
-  
-  def buscar_cliente(self):
-    try:
-        cliente = self.ventas.txtBuscarCliente.text().strip().lower()
-        query = "SELECT ID, nombre, dni FROM Clientes WHERE LOWER(nombre) LIKE ? OR LOWER(nombre) LIKE ? ORDER BY id DESC"
-        values = (f"{cliente}%", f"% {cliente}%") # Esto es tremendo soy un capo
-        datos_clientes = self.db.execute_query_fetchall(query,values)
-
-        # Limpiar la tabla antes de cargar nuevos datos
-        self.ventas.tblClientes.setRowCount(0)
-        self.ventas.tblClientes.setRowCount(len(datos_clientes))
-        for fila, item in enumerate(datos_clientes):
-            for columna, valor in enumerate(item):
-                self.ventas.tblClientes.setItem(fila, columna, QTableWidgetItem(str(valor)))
-    except Exception as e:
-        QMessageBox.critical(self.ventas, "Error", f"No se pudo buscar el cliente: {e}")
-        
-  def buscar_vehiculo(self):
-    try:
-      txt_busq = self.ventas.txtBuscarVehiculo.text().strip().lower()
-      query = "SELECT ID, marca, modelo, color, dominio FROM Vehiculos WHERE marca LIKE ? OR modelo LIKE ? OR dominio LIKE ?"
-      values = (f"%{txt_busq}%", f"%{txt_busq}%", f"%{txt_busq}%")
-      datos_vehiculos = self.db.execute_query_fetchall(query,values)
-
-      # Limpiar la tabla antes de cargar nuevos datos
-      self.ventas.tblVehiculos.setRowCount(0)
-      self.ventas.tblVehiculos.setRowCount(len(datos_vehiculos))
-      for fila, item in enumerate(datos_vehiculos):
-          for columna, valor in enumerate(item):
-              self.ventas.tblVehiculos.setItem(fila, columna, QTableWidgetItem(str(valor)))
-    except Exception as e:
-        QMessageBox.critical(self.ventas, "Error", f"No se pudo buscar el vehículo: {e}")
-    
-  def buscar_venta(self):
+  def cargar_ventas(self):
     try:
       txt_busq = self.ventas.txtBuscarVenta.text().strip().lower()
       query = "SELECT Vent.ID, C.nombre, Vent.precio_final, Vent.fecha_venta, Vehi.marca, Vehi.modelo, Vehi.dominio FROM VENTAS AS Vent INNER JOIN CLIENTES AS C ON Vent.id_cliente = C.ID INNER JOIN VEHICULOS AS Vehi ON Vent.id_vehiculo = Vehi.ID WHERE LOWER(C.nombre) LIKE ? OR LOWER(C.nombre) LIKE ? OR LOWER(Vehi.marca) LIKE ? OR LOWER(Vehi.modelo) LIKE ? OR LOWER(Vehi.dominio) LIKE ? ORDER BY Vent.ID DESC"
@@ -125,6 +48,38 @@ class Ventas():
       self.ventas.tblVentas.resizeColumnsToContents()
     except Exception as e:
       QMessageBox.critical(self.vehiculo, "Error", f"No se pudo buscar la venta: {e}")
+  
+  def cargar_clientes(self):
+    try:
+        cliente = self.ventas.txtBuscarCliente.text().strip().lower()
+        query = "SELECT ID, nombre, dni FROM Clientes WHERE LOWER(nombre) LIKE ? OR LOWER(nombre) LIKE ? ORDER BY id DESC"
+        values = (f"{cliente}%", f"% {cliente}%") # Esto es tremendo soy un capo
+        datos_clientes = self.db.execute_query_fetchall(query,values)
+
+        # Limpiar la tabla antes de cargar nuevos datos
+        self.ventas.tblClientes.setRowCount(0)
+        self.ventas.tblClientes.setRowCount(len(datos_clientes))
+        for fila, item in enumerate(datos_clientes):
+            for columna, valor in enumerate(item):
+                self.ventas.tblClientes.setItem(fila, columna, QTableWidgetItem(str(valor)))
+    except Exception as e:
+        QMessageBox.critical(self.ventas, "Error", f"No se pudo buscar el cliente: {e}")
+        
+  def cargar_vehiculos(self):
+    try:
+      txt_busq = self.ventas.txtBuscarVehiculo.text().strip().lower()
+      query = "SELECT ID, marca, modelo, color, dominio FROM Vehiculos WHERE marca LIKE ? OR modelo LIKE ? OR dominio LIKE ?"
+      values = (f"%{txt_busq}%", f"%{txt_busq}%", f"%{txt_busq}%")
+      datos_vehiculos = self.db.execute_query_fetchall(query,values)
+
+      # Limpiar la tabla antes de cargar nuevos datos
+      self.ventas.tblVehiculos.setRowCount(0)
+      self.ventas.tblVehiculos.setRowCount(len(datos_vehiculos))
+      for fila, item in enumerate(datos_vehiculos):
+          for columna, valor in enumerate(item):
+              self.ventas.tblVehiculos.setItem(fila, columna, QTableWidgetItem(str(valor)))
+    except Exception as e:
+        QMessageBox.critical(self.ventas, "Error", f"No se pudo buscar el vehículo: {e}")
 
   def seleccionar_cliente(self):
     row = self.ventas.tblClientes.currentRow()

0422db3a42a4a950a60ceca6223c583ab7d97aa8
Marcos22 <marcoscufre04@gmail.com>
2024-11-02 21:09:40 -0300

    changes

---
+++ app.py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import QApplication
+from src.controllers.inicio import Inicio
+from src.controllers.cliente import Clientes
+from src.controllers.ventas import Ventas
+from src.controllers.vehiculo import Vehiculos
+
+
+class App():
+    def __init__(self):
+        self.App = QApplication([])
+        self.inicio = Inicio()
+        self.App.exec()

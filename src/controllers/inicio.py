a1263eeeb66a4177d13be3425ec1d444e38d50ea
Gabriel <gabrieltrejo1312@gmail.com>
2024-11-28 09:24:15 -0300

    Cambios inicio

--- src/controllers/inicio.py
+++ src/controllers/inicio.py
@@ -1,5 +1,6 @@
 from PyQt5 import uic
 from controllers.cliente import Clientes
+from controllers.estadoVehiculos import EstadoVehiculos
 from controllers.vehiculo import Vehiculos
 from controllers.ventas import Ventas
 from config import base_path
@@ -16,7 +17,7 @@ class Inicio():
         self.inicio.btnClientes.clicked.connect(self.abrirClientes)
         self.inicio.btnVehiculos.clicked.connect(self.abrirVehiculos)
         self.inicio.btnVentas.clicked.connect(self.abrirVentas)
-    #    self.inicio.btnEstadoVehiculo.clicked.connect(self.abrirEstadoVehiculos)
+        self.inicio.btnEstadoVehiculo.clicked.connect(self.abrirEstadoVehiculos)
 
     def abrirVehiculos(self):
         self.vehiculos = Vehiculos()
@@ -27,5 +28,5 @@ class Inicio():
     def abrirVentas(self):
         self.ventas = Ventas()
         
-    #def abrirEstadoVehiculos(self):
-    #    self.estadovehiculos = EstadoVehiculos()
+    def abrirEstadoVehiculos(self):
+        self.estadovehiculos = EstadoVehiculos()
